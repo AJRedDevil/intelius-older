d5edfd6066c55364ef8b9b93de49fa87310ae0d9
Amrit Chhetri <amrit.chhetri@logpoint.com>
2016-01-03 16:46:15 +0545

    method for extracting data using cvs stored in database

--- using_mechanize/start.py
+++ using_mechanize/start.py
@@ -8,7 +8,7 @@ import time
 import helper
 import traceback
 
-from database import configure_logging
+from database import REThink, configure_logging
 configure_logging()
 
 import logging as log
@@ -64,10 +64,62 @@ def start(section):
     print tb
     print "Parsing completed"
 
+
+def start_from_database(section):
+    database = helper.get_database_object()
+    try:
+        field_names = database.read(
+            'csv_header', REThink.rethinkdb.row["name"] == "%s_input" % section)
+
+        field_names = list(field_names)
+        if field_names:
+            field_names = field_names[0]
+        else:
+            print "no data"
+            return
+
+        name = field_names.get('headers').get('name')
+        city = field_names.get('headers').get('city')
+        state = field_names.get('headers').get('state')
+
+        content = database.read(section)
+
+        # initiate browser
+        browser = MechanizeBrowser(section)
+        browser.start()
+
+        index = 1
+        for row in content:
+            first_name, last_name = helper.split_name(row[name])
+            city_state = "{0} , {1}".format(row[city], row[state])
+
+            try:
+                browser.search(index, first_name, last_name, city_state)
+                log.info("Parsed for First Name: '%s', Last Name: '%s' and City State: '%s' indexed: '%s'" % (
+                    first_name, last_name, city_state, index))
+                time.sleep(1)
+            except Exception, e:
+                print str(e)
+                print "Error Parsing for First Name: '%s', Last Name: '%s' and City State: '%s' indexed: '%s'" % (first_name, last_name, city_state, index)
+                tb = traceback.format_exc()
+                if tb:
+                    print tb
+            index += 1
+
+    except Exception, e:
+        print str(e)
+        tb = traceback.format_exc()
+    else:
+        tb = "No error"
+
+    print tb
+    print "Parsing completed"
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         section = sys.argv[1]
-        start(section)
+        # start(section)
+        start_from_database(section)
     else:
         print '''Usage:
 python <path_to_start.py_file>/start.py SECTION_NAME
